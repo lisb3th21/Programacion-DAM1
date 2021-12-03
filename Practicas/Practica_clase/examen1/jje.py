2426a6c9693d0f77776e71bf59d1782dfdbcd491
Lisbeth Poaquiza <91618974+lisb3th21@users.noreply.github.com>
2021-12-03 10:52:47 +0100

    -

--- Practicas/Practica_clase/examen1/jje.py
+++ Practicas/Practica_clase/examen1/jje.py
@@ -1,21 +1,11 @@
-def esPrimo(i):
-    print ('''
-           =============================================
-            Programa calculo de la conjetura de Sheldon
-           =============================================
-           ''')
-    #pedimos que nos ingresen un numero 
-    numero=input("Dame el número para el que quiere comprobar si cumple con el teorema de Sheldon: ")
-    #creamos una variable que contenga el valor booleano false
-    resultado=False
-    #si cumple las siguientes condiciones el numero será primo
-    if int(i)%int(i)==0 and int(i)%1==0 and i%2!=0:
-        resultado=True
-    elif i==2:
-        resultado=True
-        #si no cumple las condiciones anteriores no se añade nada a la lista
-    else:
-        resultado=False 
-    return resultado
+#Creamos la funcion obtenerNumMult que nos devolvera un numero multiplicado por todos sus componentes
+def obtenerNumMult(numeroMul):
+    #definimos una variable con valor 1(que es el numero neutro de la multiplicación)
+    resultadoMultiplicar=1
+    #creamos un bucle que ira multiplicando los componentes del numero
+    for i in (str(numeroMul)):
+        resultadoMultiplicar*=int(i)
+    return resultadoMultiplicar
 
-print(esPrimo(i=3))
+
+print(obtenerNumMult(numeroMul=227))

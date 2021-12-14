625638a5a5b7897fd38b50ca254f13eeee9bc5a3
Lisbeth <epoaquizacango@cifpfbmoll.eu>
2021-12-14 19:42:06 +0100

    14-12-21

--- java/java-Dam/src/practica2/ej1.java
+++ java/java-Dam/src/practica2/ej1.java
@@ -6,9 +6,7 @@ public class ej1 {
     public static void main(String[] args){
 
     String nombre = JOptionPane.showInputDialog("Ingresa tu nombre: ");
-    //Scanner entrada= new Scanner (System.in);
-    //System.out.println("Ingresa tu nombre: ");
-    //String nombre= entrada.nextLine();
+  
     System.out.println("Bienvenido "+ nombre);
 
     

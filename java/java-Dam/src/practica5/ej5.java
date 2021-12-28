267bf3cf99e3a29cbd1c7990a1c8d7da09a0a7a0
lisb3th21 <epoaquizacango@cifpfbmoll.eu>
2021-12-28 10:28:28 +0100

    28.12.21

--- java/java-Dam/src/practica5/ej5.java
+++ java/java-Dam/src/practica5/ej5.java
@@ -11,43 +11,10 @@ número que no sea mayor que el anterior. El programa termina escribiendo la lis
 */
 public class ej5 {
     public static void main(String[] args) {
-        List <Integer> listaNum= new ArrayList<Integer>();
-        Scanner entrada= new Scanner(System.in);
-        int num, longitud, ultimo;
-        System.out.println("Ingrese un numero:");
-        num=entrada.nextInt();
-        listaNum.add(num);
-        ultimo=num;
-        do{
-            System.out.println("Ingrese un número mayor a "+ultimo);
-            listaNum.add(num);
-            longitud= listaNum.size();
-            ultimo=listaNum.get(longitud);
-
-        }while(num<ultimo);
+        
         
         
 
     }
     
 }
-/*
-        while (num2<=num){
-            num2=num;
-            System.out.println("ingrese un numero mayor a "+num);
-            num=entrada.nextInt();
-            listaNum.add(num);
-        }
-        int ultimo= listaNum.size();
-        System.out.println(listaNum);
-
-        //listaNum.toArray();
-
-        String string="";
-        for (int numeros: listaNum){
-            string+=numeros;
-
-
-        }
-        System.out.println(string);
-*/

7b6b59d8c46089e420ac2170b695257040fdecd0
lisb3th21 <epoaquizacango@cifpfbmoll.eu>
2022-02-01 21:01:22 +0100

    91.01.22

--- java/java-Dam/src/practica6/ej9.java
+++ java/java-Dam/src/practica6/ej9.java
@@ -1,10 +1,34 @@
 package practica6;
 
+import java.util.Scanner;
+
+//Escribe un programa que pida dos palabras las pase como parámetro a un procedimiento y diga si riman o no. Si coinciden las tres últimas letras tiene que decir que riman. Si coinciden solo las dos últimas tiene que decir que riman un poco y si no, que no riman.
 public class ej9 {
 
-    public static boolean rima(String palabra1, String palabra2){
-boolean rima=false;
+    public static String rima(String palabra1, String palabra2) {
+        String rimaMucho = palabra1 + " y " + palabra2 + " riman mucho";
+        String rimaPoco = palabra1 + " y " + palabra2 + " riman poco";
+        palabra1 = palabra1.substring(palabra1.length() - 3, palabra1.length());
+        palabra2 = palabra2.substring(palabra2.length() - 3, palabra2.length());
+        if (palabra1.equalsIgnoreCase(palabra2)) {
+            return rimaMucho;
+        }
+        palabra1 = palabra1.substring(palabra1.length() - 2, palabra1.length());
+        palabra2 = palabra2.substring(palabra2.length() - 2, palabra2.length());
+        if (palabra1.equalsIgnoreCase(palabra2)) {
+            return rimaPoco;
+        } else {
+            return "no riman";
+        }
+    }
 
-        return rima;
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Ingrese una palaba");
+        String palabra1 = input.next();
+        System.out.println("Ingrese otra palabra: ");
+        String palabra2 = input.next();
+        System.out.println(rima(palabra1, palabra2));
+        input.close();
     }
 }
---
+++ java/java-Dam/src/practicaclase1/ejercicio.java
@@ -0,0 +1,120 @@
+package practicaclase1;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class ejercicio {
+    
+    public static void main(String[] args) {
+        
+        System.out.println("Ingrese el ejercicio que desee ejecutar: ");
+        Scanner input= new Scanner(System.in);
+        int respuesta=input.nextInt();
+        switch (respuesta){
+            case 1:ejercicioA();
+            case 2:ejercicioB();
+            case 3:ejercicioC();
+            case 4: ejercicioD();
+            case 5:ejercicioE();
+            case 6:ejercicioF();
+            case 7:ejercicioG();
+            case 8:ejercicioH();
+            case 9:ejercicioI();
+        }
+    }
+
+    public static void ejercicioA(){
+        //Leer 5 números (con bucle por favor, no pongáis 5 scanner consecutivos) y mostrarlos en el mismo orden introducido. Los 5 números se deben almacenar en un array.
+        ArrayList<Integer> listaNumeros=new ArrayList<Integer>();
+        int numero=0;
+        Scanner input= new Scanner(System.in);
+        for (int i=0; i<5;i++){
+            System.out.println("Ingrese un numero: ");
+            numero=input.nextInt();
+            listaNumeros.add(numero);            
+        }
+        System.out.println("Los numeros que ha ingresado son: ");
+        String numeros="";
+        for (int i=0; i<listaNumeros.size(); i++){
+            numeros+=listaNumeros.get(i)+"   ";
+        }
+        System.out.println(numeros);
+        input.close();
+    }
+
+
+
+    public static void ejercicioB(){
+        //Leer 5 números y mostrarlos en orden inverso al introducido.
+        ArrayList<Integer> listaNumeros=new ArrayList<Integer>();
+        int numero=0;
+        Scanner input= new Scanner(System.in);
+        for (int i=0; i<5;i++){
+            System.out.println("Ingrese un numero: ");
+            numero=input.nextInt();
+            listaNumeros.add(numero);            
+        }
+        System.out.println("Los numeros que ha ingresado son: ");
+        String numeros="";
+        for (int i=listaNumeros.size()-1; i>=0; i--){
+            numeros+=listaNumeros.get(i)+"   ";
+        }
+        System.out.println(numeros);
+        input.close();
+    }
+
+    public static void ejercicioC(){
+        //Leer 5 números por teclado y a continuación realizar la media de los números positivos, la media de los negativos y contar el número de ceros que se han introducido por teclado.
+        ArrayList<Integer> listaPositivos=new ArrayList<Integer>();
+        ArrayList<Integer> listaNegativos=new ArrayList<Integer>();
+        int numero=0;
+        int cantidadCero=0;
+        Scanner input= new Scanner(System.in);
+        for (int i=0; i<5;i++){
+            System.out.println("Ingrese un numero: ");
+            numero=input.nextInt();
+            if (numero>0){
+                listaPositivos.add(numero);
+            }else if(numero<0){
+                listaNegativos.add(numero);
+            }else{
+                cantidadCero++;
+            }
+        }
+float mediaPositivos=0f;
+        //media de los positivos
+        for (int i=0; i<listaPositivos.size(); i++){
+            mediaPositivos+=listaPositivos.get(i);
+        }
+        System.out.println("La media de los numeros positivos que ha ingresado es "+(mediaPositivos/listaPositivos.size()));
+        float mediaNegativo=0f;
+        //media de los negativos
+        for (int i=0; i<listaNegativos.size(); i++){
+            mediaNegativo+=listaNegativos.get(i);
+        }
+        System.out.println("La media de los numeros positivos que ha ingresado es "+(mediaNegativo/listaNegativos.size()));
+        System.out.println();
+
+    }
+    public static void ejercicioD(){
+
+    }
+    public static void ejercicioE(){
+
+    }
+    public static void ejercicioF(){
+
+    }
+    public static void ejercicioG(){
+
+    }
+    public static void ejercicioH(){
+
+    }
+    public static void ejercicioI(){
+
+    }
+
+
+    
+}

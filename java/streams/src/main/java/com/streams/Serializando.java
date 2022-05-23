c933921068ee45ddc07d2ca4584c64f1f7031d3e
Lisbeth <epoaquizacango@cifpfbmoll.eu>
2022-05-23 13:02:32 +0200

    jgh

--- java/streams/src/main/java/com/streams/Serializando.java
+++ java/streams/src/main/java/com/streams/Serializando.java
@@ -11,69 +11,44 @@ class Persona {
     private String apellido;
     private int edad;
 
-
-
-
-
-
     public Persona(String nombre, String apellido, int edad) {
         this.nombre = nombre;
         this.apellido = apellido;
         this.edad = edad;
     }
 
-
-
-
-
-
     @Override
     public String toString() {
         return "Persona [apellido=" + apellido + ", edad=" + edad + ", nombre=" + nombre + "]";
     }
 
-
-    
 }
 
-
-
- class Empleado extends Persona implements Serializable {
+class Empleado extends Persona implements Serializable {
     private String empresa;
     private float sueldo;
+
     public Empleado(String nombre, String apellido, int edad, String empresa, float sueldo) {
         super(nombre, apellido, edad);
-        this.empresa=empresa;
-        this.sueldo=sueldo;
-        
+        this.empresa = empresa;
+        this.sueldo = sueldo;
+
     }
-
-
-
-
-    
-
-
-
 
     @Override
     public String toString() {
         return "Empleado [empresa=" + empresa + ", sueldo=" + sueldo + "]";
-    }   
+    }
 }
 
-
-
-public  class Serializando {
+public class Serializando {
     public static void main(String[] args) {
-        
 
+        Empleado[] personal = new Empleado[3];
 
-        Empleado[] personal= new Empleado[3];
-        
-        personal[0]=new Empleado("Juan", "Llado", 23, "Brico", 2000);
-        personal[1]= new Empleado("Luis", "Sanchex", 54, "Brico", 2000);
-        personal[1]= new Empleado("Rebeca", "Toala", 32, "Brico", 1500);
+        personal[0] = new Empleado("Juan", "Llado", 23, "Brico", 2000);
+        personal[1] = new Empleado("Luis", "Sanchex", 54, "Brico", 2000);
+        personal[1] = new Empleado("Rebeca", "Toala", 32, "Brico", 1500);
 
         try {
             ObjectOutputStream write = new ObjectOutputStream(new FileOutputStream("empleados.txt"));
@@ -90,9 +65,9 @@ public  class Serializando {
             }
 
         } catch (Exception e) {
-e.printStackTrace();        }
-        
-        
+            e.printStackTrace();
+        }
+
     }
-    
+
 }
